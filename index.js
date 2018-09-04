499e574659a0e2c3ebc923fac69b6094afcd05d8
priscilafernandes <pris1@outlook.com.br>
2018-09-03 23:15:34 -0300

    ajustes na função arrayToObject

--- index.js
+++ index.js
@@ -9,18 +9,43 @@ function getLinksFromMd(str) {
   return arrayToObject(url, text); 
 }
 
-function arrayToObject(firstArray, secondArray) {
-  let obj = {};
+function arrayToObject(firstArray, secondArray) {  
   let array = [];
+  
+  firstArray.forEach(function(value) {
+    let obj = {};
+    obj['href'] = value;
+    array.push(obj);
+    secondArray.forEach(function(value) {
+      obj['text'] = value;
+      array.push(obj);
+    })
+  })
 
+/*
+  firstArray.map(function(value) {
+    let obj = {};
+    obj['href'] = value;
+    array.push(obj);
+  })
+  secondArray.map(function(value) {
+    let obj = {};
+    obj['text'] = value;
+    array.push(obj);
+  })  
+*/
+
+/*
   for (i = 0; i < firstArray.length; i++) {
+    let obj = {};
     obj['href'] = firstArray[i];
     for(j = 0; j < secondArray.length; j++) {
       obj['text'] = secondArray[j];
       array.push(obj);
     }   
   } 
-
+*/
+  
   return array;
 }
 
